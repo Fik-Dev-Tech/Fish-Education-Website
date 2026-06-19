ef7cdd0af854cf4532b35765749aad14fd0487cb
Tamale Shafik <tamaleshafik03@gmail.com>
2026-06-19 21:24:26 +0300

    Initial upload of website
    Uploading HTML, CSS, and JS files for Aqua-Harvest educational website project

---
+++ script.js
@@ -0,0 +1,9 @@
+// MOBILE MENU (basic future upgrade ready)
+console.log("AquaHarvest JS Loaded");
+
+// SIMPLE SMOOTH EFFECT
+document.querySelectorAll("a").forEach(link=>{
+link.addEventListener("click",()=>{
+console.log("Navigating...");
+});
+});
